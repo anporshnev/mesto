fdbbf74b0a86421a6fb7f986ed7acdb5028b1c4c
Andrew Porshnev <andrew.porshnev@yandex.ru>
2020-11-25 23:08:07 +0300

    feat: js добавлен закр откр popup. fix: footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-let popupOpen = document.querySelector('.profile__btn-edit');
+const profileButtonEdit = document.querySelector('.profile__btn-edit');
+const popupButtonClose= document.querySelector('.popup__icon-close');
+const popupNode = document.querySelector('.popup');
+
+profileButtonEdit.addEventListener('click', handleProfileButtonClick);
+popupButtonClose.addEventListener('click', handlePopupButtonCloseClick);
+
+function handleProfileButtonClick() {
+  popupNode.classList.add('popup_open');
+}
+
+function handlePopupButtonCloseClick() {
+  popupNode.classList.remove('popup_open');
+}
+
