4644534f10125ac85802504fbae06c1a20eb6dd7
Andrew Porshnev <andrew.porshnev@yandex.ru>
2020-11-27 11:25:22 +0300

    fix/ Пофиксен js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,30 +1,34 @@
 const profileButtonEdit = document.querySelector('.profile__btn-edit');
+
 const popupButtonClose= document.querySelector('.popup__icon-close');
 const popupNode = document.querySelector('.popup');
+
 const profileName = document.querySelector('.profile__name');
 const profileInterests = document.querySelector('.profile__text-interests');
-const popupInput = document.querySelectorAll('.popup__input');
 
-profileButtonEdit.addEventListener('click', togglePopupVisibility);
-profileButtonEdit.addEventListener('click', getValuesProfile);
-popupButtonClose.addEventListener('click', togglePopupVisibility);
+const popupInputName = document.querySelector('.popup__input_content_username');
+const popupInputInterest = document.querySelector('.popup__input_content_about');
+
+const formElement = document.querySelector('.popup__form');
 
 function togglePopupVisibility() {
   popupNode.classList.toggle('popup_open');
 }
 
 function getValuesProfile() {
-  popupInput[0].value = profileName.textContent;
-  popupInput[1].value = profileInterests.textContent;
+  popupInputName.value = profileName.textContent;
+  popupInputInterest.value = profileInterests.textContent;
 }
-
-const formElement = document.querySelector('.popup__form');
 
 function handleFormSubmit (event) {
   event.preventDefault();
-  profileName.textContent = popupInput[0].value;
-  profileInterests.textContent = popupInput[1].value;
+  profileName.textContent = popupInputName.value;
+  profileInterests.textContent = popupInputInterest.value;
   togglePopupVisibility();
 }
+
+profileButtonEdit.addEventListener('click', togglePopupVisibility);
+profileButtonEdit.addEventListener('click', getValuesProfile);
+popupButtonClose.addEventListener('click', togglePopupVisibility);
 
 formElement.addEventListener('submit', handleFormSubmit);
