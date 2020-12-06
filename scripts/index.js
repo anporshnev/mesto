7f8c36c08aa7541a4d9876e9edd45d9395a699f2
Andrew Porshnev <andrew.porshnev@yandex.ru>
2020-12-06 23:34:36 +0300

    feat: js добавлена функция инициализации карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,8 @@ const popupInputInterest = document.querySelector('.popup__input_content_about')
 
 const formElement = document.querySelector('.popup__form');
 
+const elements = document.querySelector('.elements');
+
 function togglePopupVisibility() {
   popupNode.classList.toggle('popup_open');
 }
@@ -32,3 +34,22 @@ profileButtonEdit.addEventListener('click', getValuesProfile);
 popupButtonClose.addEventListener('click', togglePopupVisibility);
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+const sendInitialElements = initialCards.forEach(function(item){
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = `Изображение места ${item.name}`;
+  cardElement.querySelector('.card__title').textContent = item.name;
+
+  return elements.append(cardElement);
+})
+
+
+
+
+
+
