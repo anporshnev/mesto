ff5bd40c35c4dfadb54a8af04c4c166bc6893259
Andrew Porshnev <andrew.porshnev@yandex.ru>
2020-12-14 18:05:59 +0300

    feat: добавлен функционал на js для popup-image

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,9 @@ const formProfile = document.querySelector('.form-profile');
 const formCard = document.querySelector('.form-card');
 
 const popupNode = document.querySelector('.popup');
-// const popupContent = document.querySelector('.popup__content');
 const popupProfile = document.querySelector('.popup-profile');
 const popupCard = document.querySelector('.popup-card');
 const cardButtonClose = document.querySelector('.card-close');
-// const likeButton = document.querySelectorAll('.card__btn-like');
-// const cardButtonRemove = document.querySelector('.card__btn-remove')
 
 const popupInputName = document.querySelector('.popup__input_content_username');
 const popupInputInterest = document.querySelector('.popup__input_content_about');
@@ -22,6 +19,8 @@ const popupInputImageLink = document.querySelector('.popup__input_content_image-
 
 const elements = document.querySelector('.elements');
 
+const popupImage = document.querySelector('.popup-image');
+const popupImageClose = document.querySelector('.image-close');
 
 let composeCard = ({name, link}) => {
   const cardTemplate = document.querySelector('#card').content;
@@ -29,13 +28,10 @@ let composeCard = ({name, link}) => {
   newCard.querySelector('.card__image').src = link;
   newCard.querySelector('.card__image').alt = `Изображение места ${name}`;
   newCard.querySelector('.card__title').textContent = name;
-  // const likeButton = newCard.querySelector('.card__btn-like');
-  // likeButton.addEventListener ('click', onLikeButton);
-  // const removeButton = newCard.querySelector('.card__btn-remove');
-  // removeButton.addEventListener('click', removeCard);
   const card = newCard.querySelector('.card');
   card.addEventListener('click', onLikeButton);
   card.addEventListener('click', removeCard);
+  card.addEventListener('click', renderPopupImage);
   return newCard;
 };
 
@@ -55,58 +51,39 @@ let addNewCard = () => {
   }
 }
 
-let onLikeButton = (e) => {
+let onLikeButton = e => {
   const eventTarget = e.target;
   if(eventTarget.classList.contains('card__btn-like')) {
     eventTarget.classList.toggle('card__btn-like_active');
   }
 }
 
-let removeCard = (e) => {
+let removeCard = e => {
   const eventTarget = e.target;
   if(eventTarget.classList.contains('card__btn-remove')) {
-    let currentTarget = e.currentTarget;
-    // console.log(1);
+    const currentTarget = e.currentTarget;
     currentTarget.removeEventListener('click', onLikeButton);
     currentTarget.removeEventListener('click', removeCard);
-
+    currentTarget.removeEventListener('click', renderPopupImage);
     currentTarget.remove();
   }
 }
 
-// let onLikeButton = (e) => {
-//   let eventTarget = e.target;
-//   eventTarget.classList.toggle('card__btn-like_active');
-// }
+let renderPopupImage = e => {
+  const popupPic = popupImage.querySelector('.popup__pic');
+  const popupPicTitle = popupImage.querySelector('.popup__pic-title');
+  const eventTarget = e.target;
+  const currentTarget = e.currentTarget;
+  if(eventTarget.classList.contains('card__image')) {
+    popupPic.src = eventTarget.src;
+    popupPic.alt = eventTarget.alt;
+    const cardImageTitle = currentTarget.querySelector('.card__title');
+    popupPicTitle.textContent = cardImageTitle.textContent;
+    openPopup(popupImage);
+  }
+}
 
 renderList();
-
-
-// const renderCards=
-//   initialCards.forEach(item => {
-//     const initialCardName = item.name;
-//     const initialCardLink = item.link;
-//     composeCard(initialCardName, initialCardLink);
-//   })
-
-// let addNewCard = () => {
-//   const cardPlaceName = popupInputPlaceName.value;
-//   const cardLink = popupInputImageLink.value;
-//   if (cardPlaceName !== '' && cardLink !== '') {
-//     composeCard(cardPlaceName, cardLink);
-//   }
-// }
-
-// let removeCard = () => elements.addEventListener('click', e => {
-//   let eventTarget = e.target;
-//   if(eventTarget.classList.contains('card__btn-remove')) {
-//     let currentTarget = eventTarget.closest('.card');
-//     currentTarget.remove();
-//   };
-// })
-// removeCard();
-
-// let onLikeButton = likeButton => likeButton.classList.toggle('card__btn-like_active');
 
 let openPopup = popup => {
   popup.classList.add('popup_open');
@@ -145,6 +122,10 @@ profileButtonClose.addEventListener('click', () => {
 
 cardButtonClose.addEventListener('click', () => {
   closePopup(popupCard);
+})
+
+popupImageClose.addEventListener('click', () => {
+  closePopup(popupImage);
 })
 
 formProfile.addEventListener('submit', handleProfileSubmit);
